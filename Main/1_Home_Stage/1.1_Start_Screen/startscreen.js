27aed32967f5c94e769ccc1a6fd84c935cc38eb4
Ze Hang Chen <164922669+Cyaze@users.noreply.github.com>
2024-08-02 11:48:20 +0100

    V0.1
    Start Screen Creation

---
+++ Main/1_Home_Stage/1.1_Start_Screen/startscreen.js
@@ -0,0 +1,9 @@
+//Redriects user to Factors page
+function gotoFactors() {
+    window.location.href = '/1_Home_Stage/1.2_Factors/factors.html';
+}
+
+//Tempeory function will change
+function showInfo() {
+    alert('This is a Evolution Simulator created by Ze Hang. Click the start button to begin the simulation.');
+}
