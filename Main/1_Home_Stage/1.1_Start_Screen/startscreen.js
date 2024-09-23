70765052ecf0986f239c6c96486c1da65aa3f835
Ze Hang Chen <164922669+Cyaze@users.noreply.github.com>
2024-09-23 15:11:19 +0100

    Delete Main/1_Home_Stage directory

--- Main/1_Home_Stage/1.1_Start_Screen/startscreen.js
+++
@@ -1,9 +0,0 @@
-//Redriects user to Factors page
-function gotoFactors() {
-    window.location.href = '/1_Home_Stage/1.2_Factors/factors.html';
-}
-
-//Tempeory function will change
-function showInfo() {
-    alert('This is a Evolution Simulator created by Ze Hang. Click the start button to begin the simulation.');
-}
